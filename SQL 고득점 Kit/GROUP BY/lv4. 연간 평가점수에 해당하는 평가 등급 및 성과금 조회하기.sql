8b865ba193ef3347e49de901602103c8d5ab8597
kweonminsung <kevin136583@gmail.com>
2024-12-14 19:06:51 +0900

    feat: 2024/12/14

---
+++ SQL 고득점 Kit/GROUP BY/lv4. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,22 @@
+-- 코드를 작성해주세요
+SELECT HR_EMPLOYEES.EMP_NO, EMP_NAME, GRADE, 
+    CASE
+        WHEN GRADE = "S" THEN SAL * 0.2
+        WHEN GRADE = "A" THEN SAL * 0.15
+        WHEN GRADE = "B" THEN SAL * 0.1
+        ELSE 0
+    END AS BONUS
+FROM (
+    SELECT EMP_NO,
+        CASE
+            WHEN AVG(SCORE)>= 96 THEN "S"
+            WHEN AVG(SCORE) >= 90 THEN "A"
+            WHEN AVG(SCORE) >= 80 THEN "B"
+            ELSE "C"
+        END AS GRADE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+) AS A
+JOIN HR_EMPLOYEES
+ON HR_EMPLOYEES.EMP_NO = A.EMP_NO
+ORDER BY HR_EMPLOYEES.EMP_NO ASC
